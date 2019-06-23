edf927982998c7d13199ffe9852a7ef6939970b3
Daniel Matetaka-Ormsby <dmat935@gmail.com>
2019-06-24 10:47:51 +1200

    setting up the home screen fragment and layout

---
+++ app/src/main/java/com/example/assignment3/HomeScreen.java
@@ -0,0 +1,28 @@
+package com.example.assignment3;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeScreen extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // setup ui components.
+
+        // setup ui actions.
+
+        // setup presenter.
+    }
+}
