06793c2f72d253a2958f01cad1102fcff122f69b
Zymbolik <48900857+Zymbolik@users.noreply.github.com>
2019-06-25 11:04:42 +1200

    Loading icon for search screen (InProgress)

--- app/src/main/java/com/example/assignment3/ui/SearchScreen.java
+++ app/src/main/java/com/example/assignment3/ui/SearchScreen.java
@@ -8,15 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.assignment3.R;
 import com.example.assignment3.core.contracts.SearchContract;
 import com.example.assignment3.core.domain.Module;
+import com.example.assignment3.core.present.ModulePresenter;
+import com.example.assignment3.ui.controls.CourseCard;
 
 import java.util.List;
 
-import java8.util.stream.StreamSupport;
+import static java8.util.stream.StreamSupport.stream;
 
 
 public class SearchScreen  extends Fragment implements SearchContract.View {
@@ -24,6 +30,11 @@ public class SearchScreen  extends Fragment implements SearchContract.View {
     private Spinner spinnerProgrammes;
     private Spinner spinnerYears;
     private Spinner spinnerSpecializations;
+    private LinearLayout searchResults;
+    private ProgressBar progressLoading;
+
+    private ScrollView scrollResults;
+    private TextView textNoResults;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -46,25 +57,45 @@ public class SearchScreen  extends Fragment implements SearchContract.View {
         spinnerSpecializations = view.findViewById(R.id.ss_spec_spinner);
         String[] specializations = getResources().getStringArray(R.array.specializations);
         spinnerSpecializations.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, specializations));
+
+        searchResults = view.findViewById(R.id.ss_results);
+        textNoResults = view.findViewById(R.id.ss_no_results);
+        scrollResults = view.findViewById(R.id.ss_search_scroll);
+        progressLoading = view.findViewById(R.id.ss_loading);
+
     }
 
     @Override
     public void displayResults(List<Module> modules) {
-
+        scrollResults.setVisibility(View.VISIBLE);
+        textNoResults.setVisibility(View.INVISIBLE);
+        stream(modules)
+                .map(ModulePresenter::new)
+                .forEach(this::appendCourseCard);
     }
 
     @Override
     public void displayNoResults() {
-
+        textNoResults.setVisibility(View.VISIBLE);
+        scrollResults.setVisibility(View.INVISIBLE);
     }
 
     @Override
     public void showLoading() {
-
+        progressLoading.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideLoading() {
+        progressLoading.setVisibility(View.INVISIBLE);
+    }
 
+    private void appendCourseCard(ModulePresenter modulePresenter) {
+        CourseCard card = new CourseCard();
+        card.setPresenter(modulePresenter);
+        getFragmentManager()
+                .beginTransaction()
+                .add(searchResults.getId(), card)
+                .commit();
     }
 }
