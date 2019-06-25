5d484a21681a93617e599531f092355e867e0e82
Daniel Matetaka-Ormsby <dmat935@gmail.com>
2019-06-26 04:25:08 +1200

    Revert "Revert "Add PageFrame and ToolBar""
    This reverts commit f366e0150666d79f306aa82ff0361d6891cc3cd3.

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -1,13 +1,85 @@
 package com.example.assignment3;
 
+import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.assignment3.ui.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static MainActivity instance;
+    private static Fragment current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Full Screen
+        getSupportActionBar().hide();
+
+        //Disable Screen Orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //Set the instance when app first run
+        if(instance == null)
+        {
+            instance = this;
+
+            //Go to the splash screen when opened
+            changePage(new SplashScreen());
+
+            return;
+        }
+    }
+
+    /***
+     * Switch to the target page without animation.
+     * @param className The class name of the layout
+     */
+    public void changePage(@NonNull final Fragment className) {
+
+        MainActivity.instance.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                current = className;
+
+                //Replace the content in the FrameLayout
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                //Invoke Method
+                ft.replace(R.id.pageFrame, current);
+                ft.commitAllowingStateLoss();
+            }
+        });
+    }
+
+    /***
+     * Switch to the target page with animation.
+     * @param timeout The time that method will wait before switch to the new page (ms)
+     * @param className The class name of the layout
+     */
+    public void changePage(final int timeout, final Fragment className) {
+
+        //showLoadScreen(); //Show the loading animation
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(timeout == -1) return;
+                try {
+                    Thread.sleep(timeout);
+                    if(className != null) {
+                        changePage(className);
+                    }
+                    //hideLoadScreen(); //Hide the loading animation
+                } catch (InterruptedException e) {  }
+
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/com/example/assignment3/ui/controls/ToolBar.java
@@ -0,0 +1,34 @@
+package com.example.assignment3.ui.controls;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.assignment3.R;
+
+public class ToolBar extends Fragment {
+
+    private static View instance;
+
+    private static int _displayHeight = 0;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.control_toolbar, container, false);
+
+        instance = view;
+
+        return view;
+    }
+}
